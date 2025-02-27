a2968c8d5d6e8cfeaa6a998f9273a1a0de4b5225
Juan Salas <142240574+jujo2005@users.noreply.github.com>
2025-02-26 19:33:39 -0500

    Update Webscrape_v1.py

--- Webscrape_v1.py
+++ Webscrape_v1.py
@@ -3,8 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# ! TODO: Figure out code to play every 5 mins.
+# ! TODO: Work on website, figure out code to play every 5 mins. Make main code a function, then call the function from another file.
 
 # Set up Selenium WebDriver
 options = Options()
@@ -65,20 +66,41 @@ confirmation = ""
 if desired_CRN not in course_dict:
     desired_CRN = input('It seems as if you entered an invalid CRN, please enter it one more time: ')
     if desired_CRN in course_dict:
-        confirmation = input(f"Is the course you want to be notified about {course_dict[desired_CRN][0]}? Please type 'Y' for Yes and 'N' for No.")
+        confirmation = input(f"Is the course you want to be notified about {course_dict[desired_CRN][0]}? Please type 'Y' for Yes and 'N' for No. ")
         if confirmation == 'Y':
-            print('Sounds good! We will notify you whenever this course has seats!')
+            print('Sounds good! We will notify you whenever this course has seats! ')
         elif confirmation == 'N':
-            print("Ahhh man, run the programa again later, we'll try to find your course. Make sure to enter the correct CRN.")
+            print("Ahhh man, run the programa again later, we'll try to find your course. Make sure to enter the correct CRN. ")
         else:
-            print("Next time, 'Y' for Yes or 'N' for No.")
+            print("Next time, 'Y' for Yes or 'N' for No. ")
     
     else: print('The CRN you entered does not appear in our system.')
 elif desired_CRN in course_dict:
-    confirmation = input(f"Is the course you want to be notified about {course_dict[desired_CRN][0]}? Please type 'Y' for Yes and 'N' for No.")
+    confirmation = input(f"Is the course you want to be notified about {course_dict[desired_CRN][0]}? Please type 'Y' for Yes and 'N' for No. ")
     if confirmation == 'Y':
-        print('Sounds good! We will notify you whenever this course has seats!')
+        print('Sounds good! We will notify you whenever this course has seats! ')
     elif confirmation == 'N':
-        print("Ahhh man, run the programa again later, we'll try to find your course. Make sure to enter the correct CRN.")
+        print("Ahhh man, run the programa again later, we'll try to find your course. Make sure to enter the correct CRN. ")
     else:
-        print("Next time, 'Y' for Yes or 'N' for No.")
+        print("Next time, 'Y' for Yes or 'N' for No. ")
+
+
+
+# Check if there are seats on the desired CRN:
+
+if confirmation == 'Y':
+    while True:
+        if int(course_dict[desired_CRN][1]) > 0:
+            print(f"Course {course_dict[desired_CRN][0]} with CRN: {desired_CRN} has {course_dict[desired_CRN][1]} seats at the moment. Register now!")
+            break
+        else:
+            print("No seats available.")
+            wait = time.sleep(2) #! This is giving an error, figure out why.
+
+
+
+
+
+
+
+
